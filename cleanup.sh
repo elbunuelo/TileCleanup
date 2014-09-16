c9875ef1cfad17f1ee8fd88c56af510745e4bb7f
elbunuelo <elbunuelo@gmail.com>
2014-09-15 23:40:44 -0500

    Ajustando Fuzz

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-
+FUZZ=.1
 find . -type f -iname "*.png" -not -path "./master/*" -print0 | while IFS= read -r -d $'\0' line; do
 
 echo "Doing stuff on $line. this shouldn't hurt much"
 #Eliminar borde blanco
-convert "$line" -bordercolor white -border 1x1 -alpha set -channel RGBA -fuzz .2% -fill none -floodfill +0+0 white -shave 1x1 "$line"
+convert "$line" -bordercolor white -border 1x1 -alpha set -channel RGBA -fuzz $FUZZ% -fill black -floodfill +0+0 white -shave 1x1 "$line"
 
 #Eliminar borde negro
-convert "$line" -bordercolor black -border 1x1 -alpha set -channel RGBA -fuzz .2% -fill none -floodfill +0+0 black -shave 1x1 "$line"
+convert "$line" -bordercolor black -border 1x1 -alpha set -channel RGBA -fuzz $FUZZ% -fill none -floodfill +0+0 black -shave 1x1 "$line"
 echo "Looks like we're done with $line, let's check if it should be exterminated"
 #Verificar si un tile está en blanco
 
